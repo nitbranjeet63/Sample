83ce10d5897fbdf6c0c992458569012688686a01
ranje <ranje@DESKTOP-9B043H0>
2019-08-07 17:31:22 +0530

    Changes in HashMapIteration Class

--- src/HashMapIteration.java
+++ src/HashMapIteration.java
@@ -23,13 +23,14 @@ public class HashMapIteration {
 		map.put(12,"HashMap");
 		System.out.println(map.size());
 		System.out.println("While Loop:");
-		Iterator itr=map.entrySet().iterator();
+		Iterator<?> itr=map.entrySet().iterator();
 		while(itr.hasNext()){
+			@SuppressWarnings("rawtypes")
 			Map.Entry me=(Map.Entry)itr.next();
 			System.out.println("Key is "+me.getKey()+"  Value is "+me.getValue());
 		}
 		System.out.println("For Loop:");
-		for(Map.Entry me2:map.entrySet()) {
+		for(@SuppressWarnings("rawtypes") Map.Entry me2:map.entrySet()) {
 			System.out.println("Key is "+me2.getKey()+"  Value is "+me2.getValue());
 		}
 	}
